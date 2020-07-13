c2f4aad3b849cca23b386c2bc1f9861e99aa5da3
hase <hm.works2@gmail.com>
2020-07-13 22:21:30 +0900

    setting firebase

---
+++ roomapp/src/views/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="user">
+    <h1>ユーザ登録ページ</h1>
+    <form>
+    <div class="form-group">
+      <label for="name">ユーザ名</label>
+      <input type="text" class="form-control" id="name">
+
+      <label for="pass">パスワード(8文字以内)</label>
+      <input type="password" name="pass" size="10" maxlength="8" id="pass">
+    </div>
+    </form>
+  </div>
+</template>
