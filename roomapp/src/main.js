2ccb9b1fdc315be3c5341a34050eec58e31eecc4
hase <hm.works2@gmail.com>
2020-07-13 22:32:55 +0900

    import jquery font awesome

--- roomapp/src/main.js
+++ roomapp/src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import jQuery from 'jquery'
+global.jquery = jQuery
+global.$ = jQuery
+window.$ = window.jQuery = require('jquery')
+
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+library.add(
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+)
+
+Vue.component('fa', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
--- roomapp/src/views/User.vue
+++ roomapp/src/views/User.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="user">
-    <h1>ユーザ登録ページ</h1>
+    <h1 class="js-title">ユーザ登録ページ</h1>
     <form>
     <div class="form-group">
       <label for="name">ユーザ名</label>
       <input type="text" class="form-control" id="name">
+      <br>
 
       <label for="pass">パスワード(8文字以内)</label>
       <input type="password" name="pass" size="10" maxlength="8" id="pass">
+      <span class="field-icon">
+        <i toggle="password-field" class="mdi mdi-eye toggle-password"></i>
+    </span>
     </div>
     </form>
   </div>
 </template>
+
+
+<script>
+export default {
+  mounted: function() {
+    $('.js-title').css('font-size','50px');
+  }
+}
+</script>
